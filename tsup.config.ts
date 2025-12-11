2fa2f065a966aded0bd5218061d5e836b554ee31
Yusuf Bhabhrawala <yusufnb@gmail.com>
2025-12-11 08:45:43 -0800

    Updated: package.json,tsup.config.ts

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.cjs'
+    }
+  }
+})
